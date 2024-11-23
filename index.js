8f292d78e8ff0f8ba5328a1dea93f32a76373df2
Brendan Smyers <bs602422@ohio.edu>
2024-11-23 17:30:33 -0500

    Better test page

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  return res.send('If you made it here, your Docker web-app is working!');
+  return res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(3000, () => {
